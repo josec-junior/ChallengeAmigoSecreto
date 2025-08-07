a637c94e29af2b743e569255ca4acb8448fa8d30
José Júnior <59178745+josec-junior@users.noreply.github.com>
2025-08-06 22:42:37 -0300

    feat: adiciona funcionalidades de adicionar amigo, visualizar lista de amigos e sortear um amigo

--- app.js
+++ app.js
@@ -1,1 +1,48 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function validarEntrada() {
+    let entrada = window.document.getElementById("amigo").value;
+    if (entrada == "") {
+        window.alert("Insira um nome válido!");
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function adicionarAmigo() {
+    let amigoValido = validarEntrada();
+    if (amigoValido) {
+        let amigo = window.document.getElementById("amigo").value;
+        amigos.push(amigo);
+    }
+    window.document.getElementById("amigo").value = "";
+    visualizarAmigos();
+}
+
+function visualizarAmigos() {
+    let lista = window.document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    if (amigos.length > 0) {
+        for (let i = 0; i < amigos.length; i++) {
+            let amigo = document.createElement("li");
+            amigo.textContent = amigos[i];
+            lista.appendChild(amigo);
+        }
+    }
+}
+
+function sortearAmigo() {
+    if (amigos.length == 0) {
+        window.alert("Não é possível sortear, pois você ainda não adicionou nenhum amigo.");
+    } else {
+        let indiceAleatorio = parseInt(Math.random() * amigos.length);
+        console.log(indiceAleatorio);
+        let amigoSorteado = amigos[indiceAleatorio];
+        let lista = window.document.getElementById("listaAmigos");
+        let resultado = window.document.getElementById("resultado");
+        lista.innerHTML = "";
+        resultado.innerHTML = `O amigo secreto sorteado é: ${amigoSorteado}`;
+    }
+}
